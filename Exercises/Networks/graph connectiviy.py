4fb00cf3982c881552a800544e8753c7f56e2109
sepante <sinasajjadi@protonmail.com>
2019-07-03 21:11:49 +0430

    labels for graph connectiviy.py added.

--- Exercises/Networks/graph connectiviy.py
+++ Exercises/Networks/graph connectiviy.py
@@ -2,14 +2,15 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 n = 100
+runNum = 100
 
 mrange = np.arange(0.10, 4, 0.25) #range for m parameter used in lognormal distribution
 connect_array = np.zeros(len(mrange)) #stores the connectivity probability for graphs
 
 for j, m in enumerate(mrange):
-    for i in range(10):
+    for i in range( runNum ):
 
-        degrees = np.rint(np.random.lognormal(mean  = m, size = 100))
+        degrees = np.rint(np.random.lognormal(mean  = m, size = n))
         #degrees should be a set of integer values
         degrees = degrees.astype(int)
         degrees *= 2 #sum of degrees should be even.
@@ -19,5 +20,7 @@ for j, m in enumerate(mrange):
         connect_array[j] += nx.is_connected(G) #returns 1 if graph is connected
 
 
-plt.title('connected probability')
+plt.xlabel('$m$')
+plt.ylabel('connectivity probability')
+plt.title('connected probability vs $m$')
 plt.plot( mrange, connect_array ,'--o')
